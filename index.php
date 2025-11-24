2da6b32724a59e0c86217c5a6ccc502015490b93
andreygonza <gonzalesandrey748@gmail.com>
2025-11-24 10:13:24 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="container">
+        <h1 class="text-center">Guess the number</h1>
+        <p><b>Rules:</b> You have to guess in the less number of attempts a number between 0 and 99 extracted casually
+            from the system.</p>
+
+    </div>
+</body>
+
+</html>
